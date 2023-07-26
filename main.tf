2d3a98d19d2e3eb4ea959a9a5cd481ea8562e6df
Jamie Taffurelli <jltaffurelli@outlook.com>
2023-07-26 13:50:52 +0100

    added public ip prefixes

--- main.tf
+++ main.tf
@@ -374,3 +374,15 @@ resource "azurerm_monitor_diagnostic_setting" "firewall_diagnostics" {
     }
   }
 }
+
+resource "azurerm_public_ip_prefix" "prefix" {
+  for_each            = { for k in var.public_ip_prefixes : k.name => k if k != null }
+  name                = each.key
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  sku                 = "Standard"
+  ip_version          = each.value.ip_version
+  prefix_length       = each.value.prefix_length
+  zones               = [1, 2, 3]
+  tags                = var.tags
+}
